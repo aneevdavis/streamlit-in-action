01e499dc6961d9a49b14424a2aafadcbea4e2c51
Aneev <aneevdavis@gmail.com>
2024-05-12 07:04:13 -0700

    Remove error handlers

--- chapter_3/unit_converter/api.py
+++ chapter_3/unit_converter/api.py
@@ -5,53 +5,36 @@ def list_quantities():
     return list(unit_config.keys())
 
 
+def list_units(quantity_name):
+    return list(unit_config[quantity_name])
+
+
 def convert_value(quantity_name, from_unit_name, to_unit_name, value):
-    try:
-        quantity = unit_config[quantity_name]
-        from_unit = quantity.units[from_unit_name]
-        to_unit = quantity.units[to_unit_name]
+    quantity = unit_config[quantity_name]
+    from_unit = quantity.units[from_unit_name]
+    to_unit = quantity.units[to_unit_name]
 
-        value_in_to_unit = _convert_value(from_unit, to_unit, value)
-        return {
-            "code": 0,
-            "result": value_in_to_unit
-        }
-
-    except KeyError:
-        _handle_errors(quantity_name, from_unit_name, to_unit_name)
+    value_in_to_unit = _convert_value(from_unit, to_unit, value)
+    return {
+        "code": 0,
+        "result": value_in_to_unit
+    }
 
 
 def convert_value_to_all_units(quantity_name, from_unit_name, value):
-    try:
-        results = {}
+    results = {}
 
-        quantity = unit_config[quantity_name]
-        from_unit = quantity.units[from_unit_name]
+    quantity = unit_config[quantity_name]
+    from_unit = quantity.units[from_unit_name]
 
-        for to_unit_name, to_unit in quantity.units.items():
-            if to_unit_name != from_unit_name:
-                results[to_unit_name] = _convert_value(from_unit, to_unit, value)
+    for to_unit_name, to_unit in quantity.units.items():
+        if to_unit_name != from_unit_name:
+            results[to_unit_name] = _convert_value(from_unit, to_unit, value)
 
-        return {
-            "code": 0,
-            "result": results
-        }
-
-    except KeyError:
-        _handle_errors(quantity_name, from_unit_name, to_unit_name)
+    return results
 
 
 def _convert_value(from_unit, to_unit, value):
     value_in_std_units = value * from_unit.value_in_standard_units
     value_in_to_unit = value_in_std_units / to_unit.value_in_standard_units
     return value_in_to_unit
-
-
-def _handle_errors(quantity_name, from_unit_name, to_unit_name):
-    if quantity_name is not None and quantity_name not in unit_config:
-        return {"code": 1, "error": f"Unknown quantity: {quantity_name}"}
-    if from_unit_name is not None and from_unit_name not in unit_config[quantity_name].units:
-        return {"code": 2, "error": f"Unknown unit: {from_unit_name}"}
-    if to_unit_name is not None and to_unit_name not in unit_config[quantity_name].units:
-        return {"code": 3, "error": f"Unknown unit: {to_unit_name}"}
-    return {"code": 100, "error": "Unknown error"}
