60a5f658685962ead9b8cd70610af963a1afdd15
Aneev <aneevdavis@gmail.com>
2024-05-14 03:21:23 -0700

    Get rid of button and make some adjustments including formatting

--- chapter_3/unit_converter/app.py
+++ chapter_3/unit_converter/app.py
@@ -2,29 +2,36 @@ import streamlit as st
 
 from backend import convert_value, list_quantities, list_units
 
-with st.sidebar:
-    quantity = st.radio("Select a quantity", list_quantities())
+def format_value(value: float, unit_abb: str, decimal_places: int = None) -> str:
+    is_rounded = decimal_places is not None
+    rounded = round(value, decimal_places) if is_rounded else value
+    formatted = format(rounded, ",")
+    return f"{formatted} {unit_abb}"
 
-units = list_units(quantity)
-st.write("# Unit Converter")
+def app():
+    st.title("Unit Converter")
 
-value = st.number_input("Value to convert")
+    quantity = st.sidebar.radio("Select a quantity", list_quantities())
+    units = list_units(quantity)
 
-from_unit_col, to_unit_col = st.columns(2)
-with from_unit_col:
-    from_unit = st.selectbox("From", units)
+    value = float(st.text_input("Value to convert", value="0"))
 
-with to_unit_col:
-    to_unit = st.selectbox("To", units)
+    from_unit_col, to_unit_col = st.columns(2)
+    with from_unit_col:
+        from_unit = st.selectbox("From", units)
+    with to_unit_col:
+        to_unit = st.selectbox("To", units, index=1)
 
-convert_button_col, reset_button_col, reverse_button_col = st.columns(3)
+    places = None
+    if st.checkbox("Round result", value=False):
+        places = st.number_input("Decimal places to round to", value=2)
 
-if st.button("Convert"):
     result = convert_value(quantity, from_unit, to_unit, value)
-    from_display_value = f"{value} {result.from_unit.abbreviation}"
-    to_display_value = f"{result.to_value} {result.to_unit.abbreviation}"
+    from_display = format_value(value, result.from_unit.abbreviation)
+    to_display = format_value(result.to_value, result.to_unit.abbreviation, places)
 
-    from_value_col, arrow_col, to_value_col = st.columns(3)
-    from_value_col.metric("From", from_display_value, delta=None)
-    arrow_col.metric("", "→")
-    to_value_col.metric("To", to_display_value, delta=None)
+    from_value_col, to_value_col = st.columns(2)
+    from_value_col.metric("From", from_display, delta=None)
+    to_value_col.metric("To", to_display, delta=None)
+
+app()
