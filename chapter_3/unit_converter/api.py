727910c27da9d0cae83fd0bc61a3bca7786d2976
Aneev <aneevdavis@gmail.com>
2024-05-14 01:40:51 -0700

    Fix data types in api.py

--- chapter_3/unit_converter/api.py
+++ chapter_3/unit_converter/api.py
@@ -6,7 +6,7 @@ def list_quantities():
 
 
 def list_units(quantity_name):
-    return list(unit_config[quantity_name])
+    return unit_config[quantity_name].units.keys()
 
 
 def convert_value(quantity_name, from_unit_name, to_unit_name, value):
@@ -15,10 +15,7 @@ def convert_value(quantity_name, from_unit_name, to_unit_name, value):
     to_unit = quantity.units[to_unit_name]
 
     value_in_to_unit = _convert_value(from_unit, to_unit, value)
-    return {
-        "code": 0,
-        "result": value_in_to_unit
-    }
+    return value_in_to_unit
 
 
 def convert_value_to_all_units(quantity_name, from_unit_name, value):
