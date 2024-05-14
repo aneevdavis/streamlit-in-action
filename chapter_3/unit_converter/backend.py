1fad34ab92f0660b81053cd5cf4adb959ee692e3
Aneev <aneevdavis@gmail.com>
2024-05-14 03:29:14 -0700

    Remove convert_to_all functionality

--- chapter_3/unit_converter/backend.py
+++ chapter_3/unit_converter/backend.py
@@ -16,25 +16,5 @@ def convert_value(quantity_name: str, from_unit_name: str, to_unit_name: str, va
     from_unit = quantity.units[from_unit_name]
     to_unit = quantity.units[to_unit_name]
 
-    value_in_to_unit = _convert_value(from_unit, to_unit, value)
-    return Result(from_unit, to_unit, value, value_in_to_unit)
-
-
-def convert_value_to_all_units(quantity_name: str, from_unit_name: str, value: float) -> dict[str, Result]:
-    results = {}
-
-    quantity = unit_config[quantity_name]
-    from_unit = quantity.units[from_unit_name]
-
-    for to_unit_name, to_unit in quantity.units.items():
-        if to_unit_name != from_unit_name:
-            converted_value = _convert_value(from_unit, to_unit, value)
-            results[to_unit_name] = Result(from_unit, to_unit, value, converted_value)
-
-    return results
-
-
-def _convert_value(from_unit: Unit, to_unit: Unit, value: float) -> float:
-    value_in_std_units = value * from_unit.value_in_standard_units
-    value_in_to_unit = value_in_std_units / to_unit.value_in_standard_units
-    return value_in_to_unit
+    value_in_to_units = value * from_unit.value_in_standard_units / to_unit.value_in_standard_units
+    return Result(from_unit, to_unit, value, value_in_to_units)
