6a6d1b4b240350eebdd54eafe8ba20fdf53ab44b
Aneev <aneevdavis@gmail.com>
2024-05-21 14:47:17 -0700

    Reformat and refresh backend.py

--- chapter_3/unit_converter/backend.py
+++ chapter_3/unit_converter/backend.py
@@ -1,6 +1,5 @@
 from unit_config import unit_config
 from result import Result
-from unit import Unit
 
 
 def list_quantities() -> list[str]:
@@ -11,10 +10,17 @@ def list_units(quantity_name) -> list[str]:
     return list(unit_config[quantity_name].units.keys())
 
 
-def convert_value(quantity_name: str, from_unit_name: str, to_unit_name: str, value: float) -> Result:
+def convert_value(
+        quantity_name: str,
+        from_unit_name: str,
+        to_unit_name: str,
+        value: float) -> Result:
     quantity = unit_config[quantity_name]
     from_unit = quantity.units[from_unit_name]
     to_unit = quantity.units[to_unit_name]
 
-    value_in_to_units = value * from_unit.value_in_standard_units / to_unit.value_in_standard_units
+    value_in_to_units = (value *
+                         from_unit.value_in_std_units /
+                         to_unit.value_in_std_units)
+
     return Result(from_unit, to_unit, value, value_in_to_units)
