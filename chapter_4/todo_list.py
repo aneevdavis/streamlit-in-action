6c72b4df049041da4b01aab10a4d4c8c1a12e692
Aneev <aneevdavis@gmail.com>
2024-06-30 12:49:59 -0700

    Replace done buttons with checkboxes

--- chapter_4/todo_list.py
+++ chapter_4/todo_list.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 if "task_list" not in st.session_state:
     st.session_state.task_list = []
+
 def add_task(task):
     st.session_state.task_list.append([task, False])
 
@@ -13,27 +14,29 @@ def delete_task(idx):
     del st.session_state.task_list[idx]
 
 task_list = st.session_state.task_list
-
 total_tasks = len(task_list)
 completed_tasks = sum(1 for _, is_done in task_list if is_done)
-display = f"{completed_tasks}/{total_tasks} done"
-st.metric("Task completion", display, delta=None)
+metric_display = f"{completed_tasks}/{total_tasks} done"
+st.metric("Task completion", metric_display, delta=None)
 
 with st.sidebar:
     task = st.text_input("Enter a task")
     if st.button("Add task"):
         add_task(task)
+        st.rerun()
 
 st.write("## To-Do List")
 for idx, [task, is_done] in enumerate(task_list):
-    task_col, change_status_col, delete_col = st.columns([0.6, 0.2, 0.2])
-    task_col.markdown(task if not is_done else f"~~{task}~~")
-
-    change_status_label = "Not done" if is_done else "Done"
-    if change_status_col.button(change_status_label, key=f"toggle_{idx}"):
+    task_col, delete_col = st.columns([0.8, 0.2])
+    label = task if not is_done else f"~~{task}~~"
+    checkbox = task_col.checkbox(label, is_done, key=f"checkbox_{idx}")
+    if checkbox and not is_done:
         toggle_done(idx)
         st.rerun()
-        
+    elif not checkbox and is_done:
+        toggle_done(idx)
+        st.rerun()
+
     if delete_col.button("Delete", key=f"delete_{idx}"):
         delete_task(idx)
         st.rerun()
