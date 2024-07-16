479b19530eebfa2fb527f69fb1f9b687086567c4
Aneev <aneevdavis@gmail.com>
2024-07-16 05:35:56 -0700

    Rename todo_list_in_progress.py to todo_list.py

--- chapter_4/todo_list.py
+++ chapter_4/todo_list.py
@@ -1,35 +1,43 @@
 import streamlit as st
+from task import Task
 
-task_list = []
+if "task_list" not in st.session_state:
+    st.session_state.task_list = []
+task_list = st.session_state.task_list
 
-def add_task(task):
-    task_list.append([task, False])
+def add_task(task_name: str):
+    task_list.append(Task(task_name))
 
-def toggle_done(idx):
-    task_list[idx][1] = not task_list[idx][1]
-
-def delete_task(idx):
+def delete_task(idx: int):
     del task_list[idx]
 
-total_tasks = len(task_list)
-completed_tasks = sum(1 for _, is_done in task_list if is_done)
-metric_display = f"{completed_tasks}/{total_tasks} done"
-st.metric("Task completion", metric_display, delta=None)
+def mark_done(task: Task):
+    task.is_done = True
+
+def mark_not_done(task: Task):
+    task.is_done = False
 
 with st.sidebar:
     task = st.text_input("Enter a task")
-    if st.button("Add task"):
+    if st.button("Add task", type="primary"):
         add_task(task)
 
-st.write("## Today's to-dos:")
-for idx, [task, is_done] in enumerate(task_list):
-    task_col, delete_col = st.columns([0.8, 0.2])
-    label = task if not is_done else f"~~{task}~~"
-    checkbox = task_col.checkbox(label, is_done)
-    if checkbox:
-        toggle_done(idx)
-    elif not checkbox:
-        toggle_done(idx)
+total_tasks = len(task_list)
+completed_tasks = sum(1 for task in task_list if task.is_done)
+metric_display = f"{completed_tasks}/{total_tasks} done"
+st.metric("Task completion", metric_display, delta=None)
 
+st.header("Today's to-dos:", divider="gray")
+for idx, task in enumerate(task_list):
+    task_col, delete_col = st.columns([0.8, 0.2])
+    label = f"~~{task.name}~~" if task.is_done else task.name
+    checked = task_col.checkbox(label, task.is_done, key=f"task_{idx}")
+    if checked and not task.is_done:
+        mark_done(task)
+        st.rerun()
+    elif not checked and task.is_done:
+        mark_not_done(task)
+        st.rerun()
     if delete_col.button("Delete", key=f"delete_{idx}"):
         delete_task(idx)
+        st.rerun()
